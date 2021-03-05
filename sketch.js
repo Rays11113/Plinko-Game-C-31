02a7c8e010726d6a75ce7b9e07a9f3ea3cd36a21
Rays11113 <74578528+Rays11113@users.noreply.github.com>
2021-03-04 16:15:12 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+var Engine = Matter.Engine;
+World = Matter.World;
+Events = Matter.Events;
+Bodies = Matter.Bodies;
+
+
+
+var particles = [];
+var plinkos = [];
+var divisions = [];
+var divisionheight = 200; 
+var score = 0; 
+
+function setup() {
+  createCanvas(700,700);
+  engine = engine.create();
+  world = engine.world;
+  ground = new Ground (width/2, height, width, 20);
+
+
+  for (var k = 0; k <=width; k = k+80){
+    divisions.push(new Divisions(k, height = divisionsHeight/2, 10, divisions));
+  }
+
+
+ for ( var j = 75; j <=width; j = j+50){
+   plinkos.push(new Plinko(j, 175));
+ }
+
+
+ for ( var j = 75; j <=width; j = j+50){
+  plinkos.push(new Plinko(j,275));
+}
+
+for ( var j = 75; j <=width; j = j+50){
+  plinkos.push(new Plinko(j,375));
+
+}
+
+}
+
+function draw() {
+  background("blue");  
+  textSize(20);
+
+  Engine.update(engine);
+  ground.display();
+
+  for ( var i = 0; i < plinkos.length; i++){
+    plinkos[i].display();
+  }
+
+  if (frameCount%60===0){
+    particles.push(new Particle(random(100, 700), 10, 10));
+    score++;
+  }
+
+  for(var j = 0; j < particles.length; j++){
+    particles[j].display();
+  }
+
+  for (var k = 0; k < divisions.length; k++) {
+    divisions[k].display;
+  }
+  drawSprites();
+}
